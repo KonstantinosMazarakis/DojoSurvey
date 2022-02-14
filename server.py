753cba801015535fbc7e1579cd9d30efb11f4a36
konstantinos mazarakis <kostasbusiness@gmail.com>
2022-02-13 19:14:22 -0500

    1

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+
+@app.route('/formResults', methods=["POST"])
+def formResults():
+    session["person"] = request.form
+    print("---------------------------------------")
+    return redirect("/results")
+
+@app.route('/results')
+def results():
+    print("---------------------------------------")
+    print(session)
+    name = session["person"]["name"]
+    location = session["person"]["location"]
+    language = session["person"]["language"]
+    text = session["person"]["text"]
+
+    return render_template("results.html", name = name, location = location, language = language, text = text)
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
